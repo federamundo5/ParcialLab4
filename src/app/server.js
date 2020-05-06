a1e4e13640bca203b8acc33ad52c151790397e73
Federico Ramundo <federicoramundo5@gmail.com>
2020-05-06 18:29:52 -0300

    LAPDSAMDF

--- src/app/server.js
+++ src/app/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(__dirname + "/angularapp"));
-app.listen(process.env.PORT || 7012);
 
-app.get('/*', function(req, res) {
-res.sendFile(path.join(__dirname + '/angularapp/index.html'));
+app.use(express.static(__dirname + '/angularapp'));
+
+app.listen(process.env.PORT || 8080);
+
+app.get('/*',function(req,res){
+  res.sendFile(path.join(__dirname + '/angularapp/index.html'));
 });
-console.log('Build successful!! Running on port:' + process.env.PORT);
+
+console.log('Finalizo');
