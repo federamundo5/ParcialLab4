43c7c996a37a5fd192b1be6cc301e300e6bb8b73
Federico Ramundo <federicoramundo5@gmail.com>
2020-05-06 17:13:28 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '' , component: BienvenidoComponent},
+  {path: 'Bienvenido' , component: BienvenidoComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
