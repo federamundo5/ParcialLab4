39ca3ea63bcae0ee8fdd07168c2f2bf94332f597
Federico Ramundo <federicoramundo5@gmail.com>
2020-05-07 21:42:24 -0300

    actgoralta

--- src/app/servicios/actores-service.service.ts
+++ src/app/servicios/actores-service.service.ts
@@ -9,6 +9,9 @@ export class ActoresServiceService {
   actores : Actores[]
   constructor( ) {
 
+this.actores=[
+  new Actores(1,"carlos","pepe","M","22/05/1996","https://concepto.de/wp-content/uploads/2018/08/persona-e1533759204552.jpg","colombia"),
+]
     localStorage.setItem('actores',JSON.stringify(this.actores));
 
   }
